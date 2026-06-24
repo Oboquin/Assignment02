6864edb28b6af482e3aa5bd4c11295a77b5dd219
orboquin <orboquin@gmail.com>
2026-06-24 01:44:59 -0700

    Finished Exercise 1

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt('Enter a Number'))
+let num2 = parseInt(prompt("Enter another number"))
+if (num1 > num2) {
+    alert(num1)
+} else if (num2 > num1) {
+        alert(num2)
+    }
+else { alert('They are equal')
+}
